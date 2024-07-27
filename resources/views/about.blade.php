545c2aea88e385668f36db77d6d179be57ce8128
khairulharu <khairulaswad719@gmail.com>
2024-07-27 18:44:55 +0800

    init

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <title>Ini Adalah Halaman About</title>
+</head>
+<body>
+     <h1>Halaman About</h1>
+
+     <p>ini adalah halaman about yang di parsing menggunakan template engine nya php</p>
+</body>
+</html>
