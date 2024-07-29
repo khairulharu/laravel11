df84a627d68670e07ba1cd3fd1d97559a4b191f0
khairulharu <khairulaswad719@gmail.com>
2024-07-29 23:02:08 +0800

    add: change mobile navbar

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,19 @@ Route::get('/about', function(){
     return view('about', ['titlePage' => 'About']);
 });
 
-Route::get('/blog', function() {
-    return view('blog', ['titlePage' => 'Blog']);
+Route::get('/posts', function() {
+    return view('posts', ['titlePage' => 'Blog', 'posts' => [
+        [
+            'title' => 'Judul Artikel 1',
+            'author' => 'Kylian Haru',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quidem placeat eos excepturi vel consequuntur porro, eius velit atque? Excepturi perferendis consequatur at nemo voluptas qui minima rerum id minus!'
+        ],
+        [
+            'title' => 'Judul Artikel 2',
+            'author' => 'Kim Haru Eyo',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quidem placeat eos excepturi vel consequuntur porro, eius velit atque? Excepturi perferendis consequatur at nemo voluptas qui minima rerum id minus!'
+        ]
+    ]]);
 });
 
 Route::get('/kontak', function() {
