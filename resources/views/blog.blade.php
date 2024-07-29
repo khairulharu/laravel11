b8fa86ca2dce42ab9b27d258a3f4ac34ba79d4f3
khairulharu <khairulaswad719@gmail.com>
2024-07-29 22:39:33 +0800

    add: artike to page blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,4 +1,13 @@
 <x-layout>
      <x-slot:slotTitlePage>{{ $titlePage }}</x-slot:slotTitlePage>
-     <h1>Ini Adalah halaman Blog</h1>
+   
+     <article class="py-8 max-w-screen-md border-b border-gray-300">
+          <h2 class="mb-1 text-3xl tracking-tighter font-bold text-gray-900">Judul Artikel 1</h2>
+          <div class="text-base text-gray-500">
+               <a href="#">Kylian Haru</a> | 1 Januari 2024
+          </div>
+          <p class="my-4 font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quidem placeat eos excepturi vel consequuntur porro, eius velit atque? Excepturi perferendis consequatur at nemo voluptas qui minima rerum id minus!</p>
+
+          <a href="#" class="font-medium text-blue-500 hover:underline">Read More &raquo;</a>
+     </article>
 </x-layout>
