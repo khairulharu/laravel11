89dcc6cfc23b6f1bb1777035cc66d566f9aed4fd
khairulharu <khairulaswad719@gmail.com>
2024-07-30 17:03:00 +0800

    add: new data to route, for parsong article data

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,13 @@ Route::get('/about', function(){
 Route::get('/posts', function() {
     return view('posts', ['titlePage' => 'Blog', 'posts' => [
         [
+            'id' => 1,
             'title' => 'Judul Artikel 1',
             'author' => 'Kylian Haru',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quidem placeat eos excepturi vel consequuntur porro, eius velit atque? Excepturi perferendis consequatur at nemo voluptas qui minima rerum id minus!'
         ],
         [
+            'id' => 2,
             'title' => 'Judul Artikel 2',
             'author' => 'Kim Haru Eyo',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quidem placeat eos excepturi vel consequuntur porro, eius velit atque? Excepturi perferendis consequatur at nemo voluptas qui minima rerum id minus!'
