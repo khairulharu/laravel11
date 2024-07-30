5d4f1aba3ab8762e356a913c3c6a69e035883d97
khairulharu <khairulaswad719@gmail.com>
2024-07-30 19:49:31 +0800

    add: new route post

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -34,7 +35,32 @@ Route::get('/posts', function() {
 });
 
 Route::get('posts/{id}', function($id){
-    dd($id);
+    $posts = [
+        [
+            'id' => 1,
+            'title' => 'Judul Artikel 1',
+            'author' => 'Kylian Haru',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quidem placeat eos excepturi vel consequuntur porro, eius velit atque? Excepturi perferendis consequatur at nemo voluptas qui minima rerum id minus!'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Judul Artikel 2',
+            'author' => 'Kim Haru Eyo',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quidem placeat eos excepturi vel consequuntur porro, eius velit atque? Excepturi perferendis consequatur at nemo voluptas qui minima rerum id minus!'
+        ],
+        [
+            'id' => 3,
+            'title' => 'Judul Artikel 3',
+            'author' => 'Kim Tai Ku',
+            'body' => 'Dolor sit amet consectetur adipisicing elit. Facere quidem placeat eos excepturi vel consequuntur porro, eius velit atque? Excepturi perferendis consequatur at nemo voluptas qui minima rerum id minus!'
+        ]
+        ];
+
+        $post = Arr::first($posts, function($post) use ($id) {
+          return $post['id'] == $id;
+        });
+
+        return view('post', ['titlePage' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/kontak', function() {
