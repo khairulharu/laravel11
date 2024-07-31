32de46a4122a883754bec131bdfdb27187cc1b20
khairulharu <khairulaswad719@gmail.com>
2024-07-31 17:40:07 +0800

    change: route post usng id to meta slug

--- routes/web.php
+++ routes/web.php
@@ -15,18 +15,21 @@ Route::get('/posts', function() {
     return view('posts', ['titlePage' => 'Blog', 'posts' => [
         [
             'id' => 1,
+            'slug' => 'judul-artikel-1',
             'title' => 'Judul Artikel 1',
             'author' => 'Kylian Haru',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quidem placeat eos excepturi vel consequuntur porro, eius velit atque? Excepturi perferendis consequatur at nemo voluptas qui minima rerum id minus!'
         ],
         [
             'id' => 2,
+            'slug' => 'judul-artikel-2',
             'title' => 'Judul Artikel 2',
             'author' => 'Kim Haru Eyo',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quidem placeat eos excepturi vel consequuntur porro, eius velit atque? Excepturi perferendis consequatur at nemo voluptas qui minima rerum id minus!'
         ],
         [
             'id' => 3,
+            'slug' => 'judul-artikel-3',
             'title' => 'Judul Artikel 3',
             'author' => 'Kim Tai Ku',
             'body' => 'Dolor sit amet consectetur adipisicing elit. Facere quidem placeat eos excepturi vel consequuntur porro, eius velit atque? Excepturi perferendis consequatur at nemo voluptas qui minima rerum id minus!'
@@ -34,30 +37,33 @@ Route::get('/posts', function() {
     ]]);
 });
 
-Route::get('posts/{id}', function($id){
+Route::get('posts/{slug}', function($slug){
     $posts = [
         [
             'id' => 1,
+            'slug' => 'judul-artikel-1',
             'title' => 'Judul Artikel 1',
             'author' => 'Kylian Haru',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quidem placeat eos excepturi vel consequuntur porro, eius velit atque? Excepturi perferendis consequatur at nemo voluptas qui minima rerum id minus!'
         ],
         [
             'id' => 2,
+            'slug' => 'judul-artikel-2',
             'title' => 'Judul Artikel 2',
             'author' => 'Kim Haru Eyo',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quidem placeat eos excepturi vel consequuntur porro, eius velit atque? Excepturi perferendis consequatur at nemo voluptas qui minima rerum id minus!'
         ],
         [
             'id' => 3,
+            'slug' => 'judul-artikel-3',
             'title' => 'Judul Artikel 3',
             'author' => 'Kim Tai Ku',
             'body' => 'Dolor sit amet consectetur adipisicing elit. Facere quidem placeat eos excepturi vel consequuntur porro, eius velit atque? Excepturi perferendis consequatur at nemo voluptas qui minima rerum id minus!'
         ]
         ];
 
-        $post = Arr::first($posts, function($post) use ($id) {
-          return $post['id'] == $id;
+        $post = Arr::first($posts, function($post) use ($slug) {
+          return $post['slug'] === $slug;
         });
 
         return view('post', ['titlePage' => 'Single Post', 'post' => $post]);
