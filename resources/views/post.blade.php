a76c730bbe9860d9f0532b857672e3406a3a4f41
khairulharu <khairulaswad719@gmail.com>
2024-08-08 22:59:07 +0800

    change: layout again

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,19 +2,21 @@
     <x-slot:slotTitlePage>{{ $titlePage }}</x-slot:slotTitlePage>
     <main class="pt-8 pb-16 lg:pt-16 lg:pb-24 dark:bg-gray-900 antialiased">
         <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
-            <article class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+            <article
+                class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
                 <header class="mb-4 lg:mb-6 not-format">
                     <address class="flex items-center mb-6 not-italic">
                         <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
                             <img class="mr-4 w-16 h-16 rounded-full"
-                                src="https://flowbite.com/docs/images/people/profile-picture-2.jpg" alt="Jese Leos">
+                                src="https://flowbite.com/docs/images/people/profile-picture-2.jpg" alt="{{ $post->author->name }}">
                             <div>
-                                <a href="/author/{{ $post->author->slug }}" rel="author"
+                                <a href="/authors/{{ $post->author->username }}" rel="author"
                                     class="text-xl font-bold text-gray-900 dark:text-white">{{ $post->author->name }}</a>
                                 <p class="text-base text-gray-500 dark:text-gray-400">Graphic Designer, educator & CEO
                                     Flowbite</p>
                                 <p class="text-base text-gray-500 dark:text-gray-400"><time pubdate
-                                        datetime="2022-02-08" title="February 8th, 2022">{{ $post->created_at->diffForHumans() }}</time></p>
+                                        datetime="2022-02-08"
+                                        title="February 8th, 2022">{{ $post->created_at->diffForHumans() }}</time></p>
                             </div>
                         </div>
                     </address>
@@ -22,7 +24,10 @@
                         class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
                         {{ $post->title }}</h1>
                 </header>
-                <p class="lead">{{ $post->body }}</p>
+                <p>{{ $post->body }}</p>
+                <a href="/posts" class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
+                   &laquo; Back To Posts
+                </a>
             </article>
         </div>
     </main>
